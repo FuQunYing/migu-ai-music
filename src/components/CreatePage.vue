070675d1c52525051ac59e8378ae5a39899190eb
t-limei.tan <t-limei.tan@pcitc.com>
2025-09-05 16:19:26 +0800

    路由修改

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -117,7 +117,7 @@ const fetchResultList = async (data = {}) => {
 
 const backCreate = () => {
   // router.back();
-  router.push({ path: 'mainPage' });
+  router.push({ path: '/mainPage' });
 };
 
 function publicVideo() {
